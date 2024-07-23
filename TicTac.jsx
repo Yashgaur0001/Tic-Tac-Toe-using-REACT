13a7971f7876e4ede437acd189b1e1637a688e1e
Yashgaur0001 <117541222+Yashgaur0001@users.noreply.github.com>
2024-07-23 23:06:58 +0530

    Add files via upload

---
+++ TicTac.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Square from './square';
+import './Board.css';
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {   
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  const Reset=()=>
+  {
+    setSquares(Array(9).fill(null));
+    if(xIsNext===false)
+    {
+        setXIsNext(true);
+    }
+    else{
+        setXIsNext(false);
+    }
+  }
+
+  return (
+    <div>
+        <h1>Tic Tac Toe</h1>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button onClick={Reset} className='reset'>Reset</button>
+    </div>
+  );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+export default Board;
